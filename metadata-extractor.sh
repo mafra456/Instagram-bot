72d93de9de8fe31b829092dc712c65f5a4b39bd9
André Correia Lacerda Mafra <andrecorreia.dcc@gmail.com>
2018-11-25 18:25:31 -0200

    Coletor de metadados de hashtags turisticas e dados coletados

---
+++ metadata-extractor.sh
@@ -0,0 +1,27 @@
+#ecoturismo
+instagram-scraper --tag trilheirasmg -m 100 --media-metadata --include-location 
+
+instagram-scraper --tag picodabandeira -m 100 --media-metadata --include-location  
+
+#festa e eventos
+instagram-scraper --tag rogerwatersbh -m 100 --media-metadata --include-location  
+
+instagram-scraper --tag solidrockfestival -m 100 --media-metadata --include-location  
+
+#praia e sol
+instagram-scraper --tag ipanemabeach -m 100 --media-metadata --include-location  
+
+instagram-scraper --tag praiadoforte -m 100 --media-metadata --include-location  
+
+#esporte
+instagram-scraper --tag atleticomineiro -m 100 --media-metadata --include-location  
+
+instagram-scraper --tag cruzeiro -m 100 --media-metadata --include-location  
+
+#cultura
+instagram-scraper --tag suassunaoautodoreinodosol -m 100 --media-metadata --include-location  
+
+instagram-scraper --tag ccbbrj -m 100 --media-metadata --include-location  
+
+instagram-scraper --tag ccbbsp -m 100 --media-metadata --include-location  
+
